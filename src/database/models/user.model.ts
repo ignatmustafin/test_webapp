93ac11cebef4a25b1523f2f9883919dccf5a4438
Ignat Mustafin <ignatmustafin77@gmail.com>
2025-02-24 10:20:16 +0300

    #1 didn't notice removed unused db validator

--- src/database/models/user.model.ts
+++ src/database/models/user.model.ts
@@ -1,4 +1,4 @@
-import {Table, Column, Model, DataType, Min} from "sequelize-typescript";
+import {Table, Column, Model, DataType} from "sequelize-typescript";
 
 @Table({
     tableName: "users",
@@ -8,7 +8,7 @@ export class User extends Model {
     @Column({ type: DataType.INTEGER, autoIncrement: true, primaryKey: true })
     id!: number;
 
-    @Min(0)
+    // min 0 added constraint in migration "20250221174200-create-users-add-row.js"
     @Column({ type: DataType.INTEGER, allowNull: false})
     balance!: number;
 }
